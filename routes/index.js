bd836c626c5dfcde38560c05601ddcab6ffbf313
Taufik Hidayat <ovick_kd@yahoo.com>
2018-05-10 13:05:52 +0700

    multer-image

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+const multer = require('multer');
+const { addImage } = require('../controllers/image_controller');
+const { sendUploadToGCS } = require('../middlewares/uploadGcs');
+const upload = multer({
+ storage  : multer.memoryStorage(),
+ limits   : {
+   fileSize: 10*1024*1024
+   }
+})
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.post('/image', upload.single('item'), sendUploadToGCS, addImage)
 
 module.exports = router;
