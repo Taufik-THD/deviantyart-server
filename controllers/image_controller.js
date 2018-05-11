bc25f5a7bbb687ce9beb6ba3c90b1bc76f0d997d
Taufik Hidayat <ovick_kd@yahoo.com>
2018-05-11 15:10:36 +0700

    final2

--- controllers/image_controller.js
+++ controllers/image_controller.js
@@ -1,25 +1,44 @@
 const Image = require('../models/picture');
+const User = require('../models/user');
+const jwt = require('jsonwebtoken')
 
 module.exports = {
 
   addImage(req, res){
 
-    const newImage = new Image({
-      user_id: '4',
-      picture_name: req.body.picture_name,
-      description: req.body.description,
-      pic_url: req.file.cloudStoragePublicUrl,
-      likes: 0
+    const token = req.body.token
+
+    jwt.verify(token, "secret", function (err, decoded) {
+
+      User.find({email: decoded.email})
+
+      .then(data => {
+        const idUser = data[0]._id
+
+        const newImage = new Image({
+          user_id: idUser,
+          picture_name: req.body.picture_name,
+          description: req.body.description,
+          pic_url: req.file.cloudStoragePublicUrl,
+          likes: 0
+        })
+
+        newImage.save()
+
+        .then(success => {
+          res.status(201).json('success save image')
+        })
+        .catch(err => {
+          res.status(400).json('failed save')
+        })
+      })
+
     })
 
-    newImage.save()
+    // const emailUser = req.body.email
+    //
 
-    .then(success => {
-      res.status(201).json('success save image')
-    })
-    .catch(err => {
-      res.status(400).json('failed save')
-    })
+
 
   },
 
